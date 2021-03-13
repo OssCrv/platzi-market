270746bd43b8d552165473e5a41999250ad69e4f
osscrv <carvajalossman@gmail.com>
2021-03-13 17:13:21 -0500

    Mapeo de tablas como clases

---
+++ src/main/java/com/carvajalossman/platzimarket/persistence/entity/Categoria.java
@@ -0,0 +1,41 @@
+package com.carvajalossman.platzimarket.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue
+    @Column(name="id_categoria")
+    private Integer idCategoria;
+
+    private String descripcion;
+
+    private Boolean estado;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
