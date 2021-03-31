89c005b0da1638942ca71f68f12278e707b4b3d0
osscrv <carvajalossman@gmail.com>
2021-03-31 13:27:40 -0500

    Corrección "@GeneratedValue(strategy = GenerationType.IDENTITY)" antes no estaba definida la strategy

--- src/main/java/com/carvajalossman/platzimarket/persistence/entity/Categoria.java
+++ src/main/java/com/carvajalossman/platzimarket/persistence/entity/Categoria.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Categoria {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_categoria")
     private Integer idCategoria;
 
