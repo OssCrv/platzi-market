db48c0d10fec4b39823e3bfb673eb8594d8614aa
osscrv <carvajalossman@gmail.com>
2021-04-10 21:24:06 -0500

    Lista seguridad (Falta implementar usuarios en base de datos) con JWT

---
+++ src/main/java/com/carvajalossman/platzimarket/web/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.carvajalossman.platzimarket.web.security;
+
+import com.carvajalossman.platzimarket.domain.service.ProjectAPIUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ProjectAPIUserDetails projectAPIUserDetails;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(projectAPIUserDetails);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
